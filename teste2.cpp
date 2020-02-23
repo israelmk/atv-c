a8e087b6e3de98a4256e9d22aa2f4184d3051781
israelm <israel98452003@gmail.com>
2020-02-23 17:31:15 -0300

    comite inicial

---
+++ teste2.cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+	
+	int num1=0,num2=0,total=0;
+	
+	printf("digite um numero:");
+	scanf("%d", &num1);
+	printf("digite outro numero:");
+	scanf("%d", &num2);
+	total=num1+num2;
+	printf("o resultado= %d", total);
+
+	return 0;
+	
+	
+	
+	
+	
+	
+}
